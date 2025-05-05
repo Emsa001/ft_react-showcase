ab4756cfde873db46d7aa9f6f6be91bda27bb7dc
Emanuel <emikscur123@gmail.com>
2025-05-05 20:27:49 +0200

    dev off

--- react/index.ts
+++ react/index.ts
@@ -103,8 +103,5 @@ export const setTitle = React.setTitle;
 /* ========================================================== */
 
 export * from "./types";
-export const IS_DEVELOPMENT = true;
+export const IS_DEVELOPMENT = false;
 export default React;
-
-console.log("public", process.env.REACT_PUBLIC_TEST);
-console.log("secret", process.env.SECRET_API_KEY)
